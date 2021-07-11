22f0d312a5da38a96b0ee6140ac69db95faa936a
MotasemK <mo3tasem66@gmail.com>
2021-07-12 01:06:50 +0300

    Adding a Cart Reducer 13

--- food-oredring-app/src/store/CartProvider.js
+++ food-oredring-app/src/store/CartProvider.js
@@ -6,7 +6,7 @@ const defaultCartState = {
   items: [],
   totalAmount: 0
 };
-
+// Here we added a cart reducer
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
     const updatedItems = state.items.concat(action.item);
